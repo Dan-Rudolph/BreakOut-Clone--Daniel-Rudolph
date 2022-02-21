35bc2c4a488049d3f3d8c40e66ec06c80cc386c9
Dan-Rudolph <99952748+Dan-Rudolph@users.noreply.github.com>
2022-02-21 13:35:43 +1100

    Update
    -small changes to rigidbody, seems to have helped with network movement

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -18,7 +18,6 @@ public class Ball : NetworkBehaviour
     Cursor cursor;
     void Start()
     {
-       
         cameraBounds = FindObjectOfType<CameraBounds>();
         renderer = GetComponent<Renderer>();
         rb = GetComponent<Rigidbody>();
@@ -87,10 +86,8 @@ public class Ball : NetworkBehaviour
         if (collision.gameObject.CompareTag("Block"))
         {
             StartCoroutine(DeflectBall(collision.GetContact(0).normal));
-            Mirror.NetworkServer.Destroy(collision.gameObject);
-            player.playerScore += 100;
-            player.SpawnNetworkObject("impact", transform.position);
-
+           
+           
         }
         if (collision.gameObject.CompareTag("Player"))
         {
